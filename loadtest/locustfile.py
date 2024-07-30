e359924c4980284480cf27ae1906f62d90a3f1c3
DaGeRe <davidgeorg_reichelt@dagere.de>
2024-07-30 12:19:33 +0200

    Add commandside to monitoring

--- loadtest/locustfile.py
+++ loadtest/locustfile.py
@@ -5,6 +5,7 @@ class HelloWorldUser(HttpUser):
     def hello_world(self):
         self.client.get("/")
         self.client.get("/api/blogposts")
+        self.client.get("/api/projects")
 
         for i in range(0,100):
           self.client.post("/api/blogpostcommands", json={"title": "Test blog entry" + str(i),
@@ -14,3 +15,12 @@ class HelloWorldUser(HttpUser):
               "category": "ENGINEERING",
               "broadcast": True,
               "publishAt": "2025-07-29T22:00:00.000Z"})
+
+        for i in range(0,100):
+            self.client.post("/api/projectcommands", json={
+                "name": "Test",
+                "repoUrl": "google.com",
+                "siteUrl": "google.com",
+                "description": "This is a test project",
+                "category": "Test Category"
+                })
